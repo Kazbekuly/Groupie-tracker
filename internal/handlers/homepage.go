2cb8b9a0225cd6eda20ae369cba3ff08c0d1d64a
Nurzhas <nurzhaskanatovbarca@gmail.com>
2022-11-14 13:11:43 +0600

    groupie-tracker

---
+++ internal/handlers/homepage.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+	"tracker/internal/utils"
+)
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		Err(w, http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		Err(w, http.StatusNotFound)
+		return
+	}
+	templ, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		Err(w, http.StatusNotFound)
+		log.Println("home page", err)
+		return
+	}
+
+	artists, errs := utils.GetData()
+	if errs != nil {
+		Err(w, http.StatusInternalServerError)
+		return
+	}
+	artists.FoundArtists = artists.Artists
+	err = templ.Execute(w, &artists)
+	if err != nil {
+		Err(w, http.StatusNotFound)
+		return
+	}
+}
